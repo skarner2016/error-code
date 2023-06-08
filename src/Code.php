c2b8f178116c979521081bbbef1f3d6532f753fb
skarner2016 <skarner2016@gmail.com>
2023-06-08 14:37:51 +0800

    project init

---
+++ src/Code.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Skarner2016\ErrorCode;
+
+/**
+ * @desc
+ * @author  skarner <2023-06-08 14:29>
+ */
+class Code
+{
+    // system error
+    const SUCCESS           = 200;
+    const UNKNOWN           = 500;
+    const PERMISSION_DENIED = 403;
+}
